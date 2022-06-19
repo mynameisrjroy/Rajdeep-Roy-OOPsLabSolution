2649d62ded853616df86efa5b961a459db9f1130
Rajdeep Roy <80695834+mynameisrjroy@users.noreply.github.com>
2022-06-19 19:24:40 +0530

    Delete GL Lab 1 directory

--- GL Lab 1/src/credential/Credential_main.java
+++
@@ -1,8 +0,0 @@
-package credential;
-
-public class Credential_main  {
-    public static void main(String[] args) {
-    	CredentialService cred = new CredentialService("Rajdeep", "Roy");
-        System.out.println(cred.showCredentials());
-    }
-}
